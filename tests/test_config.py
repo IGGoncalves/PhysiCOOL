100bfde52331d06039692885eb02a401b7a2755c
Inês G Gonçalves <m.ines.ggoncalves@gmail.com>
2022-01-27 17:22:06 +0100

    :construction:  Added small test for config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,11 @@
+"""Script to test the config module of the PhysiCOOL package."""
 import unittest
 
 from physicool import config
 
 
-class MyTestCase(unittest.TestCase):
-    def get_cell_definition_list(self):
+class PhysiCellConfigTst(unittest.TestCase):
+    def test_get_cell_definition_list(self):
         xml_data = config.ConfigFileParser()
         cell_list = xml_data.cell_definitions_list
         self.assertEqual(cell_list, ["default"])
